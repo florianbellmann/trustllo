526f8e8c4d61fe990e17b4e506ab2db2531cb2d5
Florian Jüsten <florian.juesten@fjmbp.fritz.box>
2022-10-18 19:43:08 +0200

    more formats

--- src/trello/mod.rs
+++ src/trello/mod.rs
@@ -1,9 +1,8 @@
 use std::env;
-use std::io::Read;
 
 pub fn ApiConnector() {
-    let apiKey = env::var("API_KEY").is_ok();
-    let apiToken = env::var("API_TOKEN").is_ok();
+    let _apiKey = env::var("API_KEY").is_ok();
+    let _apiToken = env::var("API_TOKEN").is_ok();
     // TODO: break and display message if env not present. do I need a test here?
     // the test is rather necessary for "load data initially", this implies reading the envs.
     // Then I need to add test envs for the tests to pass, right?
@@ -20,8 +19,6 @@ pub fn ApiConnector() {
 
 #[cfg(test)]
 mod tests {
-    use std::env;
-
     // #[test]
     // fn load_lists_from_board() {}
 
