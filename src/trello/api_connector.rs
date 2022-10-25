edc5900375fa27aedec9baff6288a69c8db0d0f7
florianjuesten <flojuesten@gmail.com>
2022-10-25 22:19:28 +0200

    ported function definitions

--- src/trello/api_connector.rs
+++ src/trello/api_connector.rs
@@ -1,4 +1,5 @@
-
+use std::env;
+use std::error::Error;
 
 use reqwest::Method;
 
@@ -18,25 +19,132 @@ impl ApiConnector {
         ApiConnector {}
     }
 
-    // TODO: refatr to the real error type
-    pub async fn loadall(&self) -> Result<(), Box<dyn std::error::Error>> {
-        // TODO: break and display message if env not present. do I need a test here?
-        // the test is rather necessary for "load data initially", this implies reading the envs.
-        // Then I need to add test envs for the tests to pass, right?
+    pub async fn init(&self) {}
 
-        // println!("{}", apiKey);
-        // println!("{}", apiToken);
-
-        // println!("{:#?}", resp);
-
-        self.make_request(Endpoint::CARDS, Method::GET, "").await;
-        Ok(())
+    // boards
+    // ----------------------------------------------------------------------------------------------------------------
+    pub async fn get_boards(&self, member_id: &str) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
     }
 
-    pub fn get_boards() {}
-    pub fn get_lists() {}
-    pub fn get_cards() {}
+    // lists
+    // ----------------------------------------------------------------------------------------------------------------
+    pub async fn get_lists_on_board(&self, board_id: &str) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
+    }
+    // pub async fn get_lists_on_board_by_filter(
+    //     &self,
+    //     board_id: &str,
+    //     filter: &str,
+    // ) -> Result<(), Box<dyn Error>> {
+    //     todo!("Not implemented yet");
+    // }
+    pub async fn get_labels_for_board(&self, board_id: &str) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
+    }
+    pub async fn get_actions_on_board(&self, board_id: &str) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
+    }
+    pub async fn add_label_to_card(
+        &self,
+        card_id: &str,
+        label_id: &str,
+    ) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
+    }
+    pub async fn delete_label_from_card(
+        &self,
+        card_id: &str,
+        label_id: &str,
+    ) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
+    }
+    pub async fn add_due_date_to_card(
+        &self,
+        card_id: &str,
+        date_value: &str,
+    ) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
+    }
 
+    // cards
+    // ----------------------------------------------------------------------------------------------------------------
+    pub async fn get_card(&self, board_id: &str, card_id: &str) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
+    }
+    // TODO: refatr to the real error type
+    pub async fn get_cards_for_list(&self, list_id: &str) -> Result<(), Box<dyn Error>> {
+        self.make_request(Endpoint::CARDS, Method::GET, "").await;
+        Ok(())
+        // todo!("Not implemented yet");
+    }
+    // pub async fn get_cards_on_board(&self, board_id: &str) -> Result<(), Box<dyn Error>> {
+    //     todo!("Not implemented yet");
+    // }
+    pub async fn add_card(
+        &self,
+        name: &str,
+        description: &str,
+        list_id: &str,
+    ) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
+    }
+    pub async fn add_checklist_to_card(
+        &self,
+        card_id: &str,
+        name: &str,
+    ) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
+    }
+    pub async fn get_checklists_on_card(&self, card_id: &str) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
+    }
+    pub async fn add_item_to_checklist(
+        &self,
+        check_list_id: &str,
+        name: &str,
+        pos: &str,
+    ) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
+    }
+    pub async fn update_card(
+        &self,
+        card_id: &str,
+        field: &str,
+        value: &str,
+    ) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
+    }
+    pub async fn update_checklist(
+        &self,
+        checklist_id: &str,
+        field: &str,
+        value: &str,
+    ) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
+    }
+    pub async fn update_card_name(&self, card_id: &str, name: &str) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
+    }
+    pub async fn update_card_description(
+        &self,
+        card_id: &str,
+        description: &str,
+    ) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
+    }
+    pub async fn delete_card(&self, card_id: &str) -> Result<(), Box<dyn Error>> {
+        todo!("Not implemented yet");
+    }
+    // pub async fn update_card_list(
+    //     &self,
+    //     card_id: &str,
+    //     list_id: &str,
+    // ) -> Result<(), Box<dyn Error>> {
+    //     todo!("Not implemented yet");
+    // }
+
+    // ----------------------------------------------------------------------------------------------------------------
     // TODO: pass also params
     async fn make_request(
         &self,
