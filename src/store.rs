4c7dbcbfba979b325d4538e46acd41f43cc0803e
florianjuesten <flojuesten@gmail.com>
2022-11-14 20:32:52 +0100

    make storedata fields public

--- src/store.rs
+++ src/store.rs
@@ -5,7 +5,7 @@ use serde::{Deserialize, Serialize};
 // TODO: Store data and the props of the the store are the same. with the difference of options
 #[derive(Debug, Serialize, Deserialize)]
 pub struct StoreData {
-    updated: String, //TODO: proper date handling
-    boards: Vec<Board>,
-    lists: Vec<List>,
+    pub updated: String, //TODO: proper date handling
+    pub boards: Vec<Board>,
+    pub lists: Vec<List>,
 }
