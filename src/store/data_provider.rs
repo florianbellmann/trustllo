67b071c94d763be0017847e00ff14731a26e9daa
florianjuesten <flojuesten@gmail.com>
2022-12-29 19:52:21 +0100

    first version of the data provider

--- src/store/data_provider.rs
+++ src/store/data_provider.rs
@@ -1,9 +1,9 @@
 use anyhow::Result;
-use log::{error, info, warn};
+use log::{error, warn};
 
 use crate::trello::{api_connector::ApiConnector, Board, Card, List};
 
-use super::store::{self, Store};
+use super::store::Store;
 
 pub struct DataProvider {
     api_connector: ApiConnector,
@@ -36,25 +36,27 @@ impl DataProvider {
     // --------------------------------------------------------------------------------------------
     pub async fn get_boards(&mut self) -> &Vec<Board> {
         if let 0 = self.store.boards.len() {
-            warn!("No boards found in store. Loading from api.");
+            warn!("No boards found in store. Loading from API.");
             match self.api_connector.get_boards().await {
                 Ok(boards) => {
                     self.store.boards = boards;
                     self.store.current_board_index = 0;
                 }
-                Err(e) => error!("Error while loading boards from api: {}", e),
+                Err(e) => error!("Error while loading boards from API: {}", e),
             }
         }
         self.store.boards.as_ref()
     }
     pub async fn get_current_board_index(&mut self) -> usize {
         if let 0 = self.store.boards.len() {
+            warn!("No current board index found in store. Loading from API.");
             self.get_boards().await;
         }
         self.store.current_board_index
     }
     pub async fn get_current_board(&mut self) -> &Board {
         if let 0 = self.store.boards.len() {
+            warn!("No current board found in store. Loading from API.");
             self.get_boards().await;
         }
         self.store.get_current_board()
@@ -68,7 +70,7 @@ impl DataProvider {
     // --------------------------------------------------------------------------------------------
     pub async fn get_current_lists(&mut self) -> &Vec<List> {
         if let 0 = self.store.current_lists.len() {
-            warn!("No lists found in store. Loading from api.");
+            warn!("No lists found in store. Loading from API.");
             match self
                 .api_connector
                 .get_lists_on_board(self.store.get_current_board().id.as_str())
@@ -78,13 +80,14 @@ impl DataProvider {
                     self.store.current_lists = lists;
                     self.store.current_list_index = 0;
                 }
-                Err(e) => error!("Error while loading lists from api: {}", e),
+                Err(e) => error!("Error while loading lists from API: {}", e),
             }
         }
         self.store.current_lists.as_ref()
     }
     pub async fn get_current_list_index(&mut self) -> usize {
         if let 0 = self.store.current_lists.len() {
+            warn!("No current list index found in store. Loading from API.");
             self.get_current_lists().await;
         }
         self.store.current_list_index
@@ -99,7 +102,7 @@ impl DataProvider {
         match self.api_connector.get_lists_on_board(board_id).await {
             Ok(lists) => lists,
             Err(e) => {
-                error!("Error while loading lists from api: {}", e);
+                error!("Error while loading lists from API: {}", e);
                 vec![]
             }
         }
@@ -112,30 +115,35 @@ impl DataProvider {
     // cards
     // --------------------------------------------------------------------------------------------
     pub async fn get_current_cards(&mut self) -> &Vec<Card> {
-        todo!();
-        // if self.store.current_cards.is_none()
-        //     || self.store.current_cards.as_ref().unwrap().is_empty()
-        // {
-        //     warn!("No cards found in store. Loading from api.");
-        //     // let id = self.get_current_list().await.id.as_str();
-        //     match self.api_connector.get_cards_on_list("2").await {
-        //         Ok(cards) => {
-        //             self.store.current_cards = Some(cards);
-        //             self.store.current_card_index = Some(0);
-        //         }
-        //         Err(e) => error!("Error while loading cards from api: {}", e),
-        //     }
-        // }
-
-        // self.store.current_cards.unwrap().as_ref()
+        if self.store.current_cards.is_none()
+            || self.store.current_cards.as_ref().unwrap().is_empty()
+        {
+            warn!("No cards found in store. Loading from API.");
+            // let id = self.get_current_list().await.id.as_str();
+            match self.api_connector.get_cards_on_list("2").await {
+                Ok(cards) => {
+                    self.store.current_cards = Some(cards);
+                    self.store.current_card_index = Some(0);
+                }
+                Err(e) => error!("Error while loading cards from API: {}", e),
+            }
+        }
+        self.store.current_cards.as_ref().unwrap()
     }
-    pub async fn get_current_card_index() -> usize {
-        todo!("not implemented");
+    pub async fn get_current_card_index(&mut self) -> usize {
+        if self.store.current_cards.is_none()
+            || self.store.current_cards.as_ref().unwrap().is_empty()
+        {
+            warn!("No card index found. Reloading current cards from API.");
+            self.get_current_cards().await;
+        }
+        self.store.current_card_index.unwrap_or(0)
     }
     pub async fn get_current_card(&mut self) -> &Card {
         if self.store.current_cards.is_none()
             || self.store.current_cards.as_ref().unwrap().is_empty()
         {
+            warn!("No current card found in store. Loading from API.");
             self.get_current_cards().await;
         }
         self.store.get_current_card().unwrap()
@@ -143,58 +151,93 @@ impl DataProvider {
     pub async fn get_last_card(&self) -> Option<&Card> {
         self.store.last_card.as_ref()
     }
-    pub async fn get_cards_on_list(&self, list_id: &str) -> Result<Vec<Card>> {
-        todo!("not implemented");
+    pub async fn get_cards_on_list(&self, list_id: &str) -> Vec<Card> {
+        match self.api_connector.get_cards_on_list(list_id).await {
+            Ok(cards) => cards,
+            Err(e) => {
+                error!("Error while loading cards from API: {}", e);
+                vec![]
+            }
+        }
     }
-    pub async fn get_card_by_id(&self, card_id: &str) -> Result<Card> {
-        todo!("not implemented");
+    pub async fn get_card_by_id(&mut self, card_id: &str) -> Card {
+        if self.store.current_cards.is_none() {
+            warn!("No current cards not found in store. Loading from API.");
+            self.get_current_cards().await;
+        }
+        if let card = self
+            .store
+            .current_cards
+            .as_ref()
+            .unwrap()
+            .iter()
+            .find(|c| c.id == card_id)
+        {
+            card.unwrap().clone()
+        } else {
+            warn!("Card not found in current cards. Loading from API.");
+            match self.api_connector.get_card_by_id(card_id).await {
+                Ok(card) => card,
+                Err(e) => {
+                    error!("Error while loading card from API: {}", e);
+                    self.store.current_cards.as_ref().unwrap()[0].clone()
+                }
+            }
+        }
     }
 
     pub async fn set_current_card_index(&mut self, index: usize) {
-        todo!("not implemented");
+        self.store.set_current_card_index(index)
     }
     pub async fn set_current_cards(&mut self, cards: &Vec<Card>) {
-        todo!("not implemented");
+        self.store.set_current_cards(cards)
     }
     pub async fn set_last_card(&mut self, card: &Card) {
-        todo!("not implemented");
+        self.store.set_last_card(card)
     }
 
     pub async fn add_card_to_list(
         &self,
-        name: &str,
-        description: &str,
-        list_id: &str,
+        _name: &str,
+        _description: &str,
+        _list_id: &str,
     ) -> Result<Card> {
-        todo!("not implemented");
+        self.api_connector
+            .add_card(_name, _description, _list_id)
+            .await
+        //TODO: missing is the creation of a card in the store that gets added to the current_cards
     }
     pub async fn archive_card(&self, card_id: &str) -> Result<Card> {
-        todo!("not implemented");
+        self.api_connector.archive_card(card_id).await
     }
     pub async fn unarchive_card(&self, card_id: &str) -> Result<Card> {
-        todo!("not implemented");
+        self.api_connector.unarchive_card(card_id).await
     }
     // pub async fn add_checklist_to_card(&self, _card_id: &str, _name: &str) -> Result<()> {}
     // pub async fn get_checklists_on_card(&self, _card_id: &str) -> Result<()> {}
     // pub async fn add_item_to_checklist(}
     pub async fn update_card(&self, card_id: &str, field: &str, value: &str) -> Result<Card> {
-        todo!("not implemented");
+        self.api_connector.update_card(card_id, field, value).await
     }
     // pub async fn update_checklist(}
     pub async fn update_card_description(&self, card_id: &str, description: &str) -> Result<Card> {
-        todo!("not implemented");
+        self.api_connector
+            .update_card_description(card_id, description)
+            .await
     }
     pub async fn update_card_title(&self, card_id: &str, title: &str) -> Result<Card> {
-        todo!("not implemented");
+        self.api_connector.update_card_title(card_id, title).await
     }
     pub async fn update_card_due_date(&self, card_id: &str, date_value: &str) -> Result<Card> {
-        todo!("not implemented");
+        self.api_connector
+            .update_card_due_date(card_id, date_value)
+            .await
     }
 
     // actions
     // --------------------------------------------------------------------------------------------
     pub async fn nuke_all(&mut self) -> Result<()> {
-        todo!("not implemented");
+        self.store.nuke_all()
     }
 }
 
