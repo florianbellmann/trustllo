ab9a0626cd24ff7ea7b9495fc865328ac5271c8e
florianjuesten <flojuesten@gmail.com>
2023-01-09 16:28:58 +0100

    log4rs

--- src/utils/logger.rs
+++ src/utils/logger.rs
@@ -13,45 +13,47 @@ use log4rs::{
 
 // TODO: the panics are only logged to the console not the file
 //TODO: use tui logger or env logger instead
+// or stay with the new log4rs config?
 pub struct Logger {}
 impl Logger {
     pub fn init() {
-        let level = log::LevelFilter::Info;
-        let file_path = "log/app.log";
+        log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+        // let level = log::LevelFilter::Info;
+        // let file_path = "log/app.log";
 
-        // Build a stderr logger.
-        let stderr = ConsoleAppender::builder().target(Target::Stderr).build();
+        // // Build a stderr logger.
+        // let stderr = ConsoleAppender::builder().target(Target::Stderr).build();
 
-        // Logging to log file.
-        let logfile = FileAppender::builder()
-            // Pattern: https://docs.rs/log4rs/*/log4rs/encode/pattern/index.html
-            .encoder(Box::new(PatternEncoder::new(
-                "{d(%+)(utc)} [{f}:{L}] {h({l})}: {m}{n}",
-            )))
-            .build(file_path)
-            .unwrap();
+        // // Logging to log file.
+        // let logfile = FileAppender::builder()
+        //     // Pattern: https://docs.rs/log4rs/*/log4rs/encode/pattern/index.html
+        //     .encoder(Box::new(PatternEncoder::new(
+        //         "{d(%+)(utc)} [{f}:{L}] {h({l})}: {m}{n}",
+        //     )))
+        //     .build(file_path)
+        //     .unwrap();
 
-        // Log Trace level output to file where trace is the default level
-        // and the programmatically specified level to stderr.
-        let config = Config::builder()
-            .appender(Appender::builder().build("logfile", Box::new(logfile)))
-            .appender(
-                Appender::builder()
-                    .filter(Box::new(ThresholdFilter::new(level)))
-                    .build("stderr", Box::new(stderr)),
-            )
-            .build(
-                Root::builder()
-                    .appender("logfile")
-                    .appender("stderr")
-                    .build(LevelFilter::Debug),
-            )
-            .unwrap();
+        // // Log Trace level output to file where trace is the default level
+        // // and the programmatically specified level to stderr.
+        // let config = Config::builder()
+        //     .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        //     .appender(
+        //         Appender::builder()
+        //             .filter(Box::new(ThresholdFilter::new(level)))
+        //             .build("stderr", Box::new(stderr)),
+        //     )
+        //     .build(
+        //         Root::builder()
+        //             .appender("logfile")
+        //             .appender("stderr")
+        //             .build(LevelFilter::Debug),
+        //     )
+        //     .unwrap();
 
-        // Use this to change log levels at runtime.
-        // This means you can change the default log level to trace
-        // if you are trying to debug an issue and need more logs on then turn it off
-        // once you are done.
-        let _handle = log4rs::init_config(config);
+        // // Use this to change log levels at runtime.
+        // // This means you can change the default log level to trace
+        // // if you are trying to debug an issue and need more logs on then turn it off
+        // // once you are done.
+        // let _handle = log4rs::init_config(config);
     }
 }
